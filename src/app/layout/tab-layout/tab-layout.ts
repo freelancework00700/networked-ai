1a4c80cdc0a15169662df013ca6ceaa1f85e2766
hardik <hrsanghani157@gmail.com>
2025-12-11 10:48:49 +0530

    style: bottom bar ui

--- src/app/layout/tab-layout/tab-layout.ts
+++ src/app/layout/tab-layout/tab-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { IonTabs, IonLabel, IonTabBar, IonTabButton, IonRouterLink } from '@ionic/angular/standalone';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IonTabs, IonLabel, IonTabBar, IonTabButton, IonRouterLink, IonIcon } from '@ionic/angular/standalone';
+import { InputIcon } from 'primeng/inputicon';
 
 @Component({
   selector: 'tab-layout',
   styleUrl: './tab-layout.scss',
   templateUrl: './tab-layout.html',
-  imports: [IonTabs, IonLabel, IonTabBar, RouterLink, IonTabButton, IonRouterLink]
+  imports: [IonIcon, IonTabs, IonLabel, IonTabBar, RouterLink, RouterLinkActive, IonTabButton, IonRouterLink, InputIcon]
 })
 export class TabLayout {}
